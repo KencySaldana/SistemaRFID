8b58f60ff7139c2104490560208a0bcce22b84f2
KencySaldana <2030024@upv.edu.mx>
2023-10-07 17:57:58 -0600

    titulos

--- sistemaRFID/resources/views/users/index.blade.php
+++ sistemaRFID/resources/views/users/index.blade.php
@@ -5,13 +5,12 @@
     <div class="overflow-x-auto">
         <div
             class="min-w-screen min-h-screen bg-gray-100 flex items-center justify-center font-sans overflow-hidden">
-            <!-- Botón agregado arriba de la card y cargado a la derecha -->
-            <h1 class="absolute font-black top-20 right-0  mr-[675px] mt-180 px-3 py-3  tracking-wider text-black rounded-full h">TABLA DE USUARIOS</h1>
             <a
                 class="absolute top-0 right-0 bg-blue-600 mt-32 mr-24 px-5 py-2 shadow-sm tracking-wider text-white rounded-full hover:bg-blue-500 m-4"
                 type="button" aria-label="like" href="{{route('user.create')}}">Agregar usuario</a>
             <div class="w-full lg:w-5/6">
-                <div class="bg-white shadow-md rounded my-6">
+                <h1 class="justify-center items-center  px-3 py-3  tracking-wider text-black ">TABLA DE USUARIOS</h1>
+                <div class="bg-white shadow-md rounded my-6">      
                     <table class="min-w-max w-full table-auto">
                         <thead>
                             <tr class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
